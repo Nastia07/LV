bd380aec1f9ecb7d5c788cfa3107c2143b92df8c
Nastia_Chulupyn <nculupin@gmail.com>
2022-03-28 20:45:00 +0300

    Added twitter link and new block about Germany

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-document.addEventListener("DOMContentLoaded", function (event) {});
+document.addEventListener("DOMContentLoaded", function (event) {
+  const twitter_link = document.querySelector(".twitter__link");
+  function setTwiterLink() {
+    const data = twitter_link.dataset.text.replaceAll(" ", "%20");
+    twitter_link.href = "https://twitter.com/intent/tweet?text=".concat(data);
+  }
+
+  setTwiterLink();
+});
